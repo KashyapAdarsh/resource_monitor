a81297af589f8209d2e7a7f35a95e41a0f9f933d
Arunesh Pandey <parunesh@parunesh-a01.vmware.com>
2018-04-19 12:14:51 -0700

    Fixed UI

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ template_dir = os.path.join(top_dir, 'templates')
 static_dir = os.path.join(top_dir, 'static')
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="../static", template_folder='../templates')
 
 @app.route('/')
 def landing():
