9da20c7180c6e0274d582fee418336db6ca396f0
Arunesh Pandey <parunesh@parunesh-a01.vmware.com>
2018-04-19 10:17:22 -0700

    Flask Setup

---
+++ app/app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+from flask import Flask, jsonify
+import docker
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return ''
+
+
+@app.route('/start_containers')
+def start_container():
+    """
+    Gets the list of host and container names and starts them
+    """
+    logs = []
+    f = open("config.txt", "r")
+    if not f:
+        return "Config file not found!!"
+    args = {'Image': 'ubuntu'}
+    for configline in f.readlines():
+        logline = ""
+        url, container_name = configline.strip().split(' ')
+        if "http://" not in url:
+            url = "http://" + url
+
+        # For each of the entries, do-
+        # 1. Create an image
+        # 2. Create a container
+        # 3. Start the container
+
+        #### 1. Create an image
+        image_name = "hello-world"
+        img_url = url + "/images/create?fromImage=" + image_name + "&tag=latest"
+        response = requests.post(img_url)
+        if response.status_code == 200:
+            logline = "Created the image - " + image_name
+            logs.append(logline)
+
+            #### 2. Create the container.
+            #### Delete all the stopped containers before creating to avoid conflict
+            del_container = url + "/containers/prune"
+            response = requests.post(del_container)
+            container_url = url + "/containers/create?name=" + container_name
+            response = requests.post(container_url, json= {"Image": image_name})
+            if response.status_code == 201:
+                container_id = response.json()["Id"]                
+                logline = "Created the container with Id- " + container_id
+                logs.append(logline)
+                
+                
+                #### 3. Start the container
+                container_start_url = url + "/containers/" + container_id + "/start"
+                response = requests.post(container_start_url)
+                if response.status_code == 204:
+                    ### All is well
+                    logline = "Started the container - " + container_name
+                    logs.append(logline)
+                    
+                else:
+                    logline = "Couldn't start container with id - " + container_start_url + " Response code - " + str(response.status_code)
+                    logs.append(logline)
+                    
+                    
+            else:
+                logline = "Container creation failed for - " + container_url + " Response code - " + str(response.status_code)
+                logs.append(logline)
+                
+                
+        else:
+            logline = "Image creation failed for - " + img_url  + " Response code - " + str(response.json())
+            logs.append(logline)            
+    return jsonify(logs)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
